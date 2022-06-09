7b47acffe2205ba6e0ccbb17576396403190bd0c
Agnes Beviz <agnes.beviz@gmail.com>
2022-06-09 23:02:45 +0100

    human cat dog years test and function

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,7 @@
-const humanCatDogYears = number => {};
-
+const humanCatDogYears = number => {
+  if (number === 1) { return [1, 15, 15];}
+  if (number === 2) { return [2, 24, 24];}
+  return [number, 24 + (number-2)*4 , 24 + (number-2)*5];
+  
+};
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,22 @@
 const { humanCatDogYears } = require("../src");
 
-// Look Ma, no handlebars!!!
+describe("humanCatDogYears", () => {
+  it("returns an array of numbers with the age in human, cat, and dog years ", () => {
+    expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+    expect(humanCatDogYears(2)).toEqual([2, 24, 24]);
+    expect(humanCatDogYears(17)).toEqual([17, 84, 99]);
+  });
+});
+
+
+/*Cat Years
+
+15 cat years for first human year
++9 cat years for second human year
++4 cat years for each human year after that
+
+Dog Years
+
+15 dog years for first year
++9 dog years for second year
++5 dog years for each year after that*/
